c0c1904df8d1ee8512cb689b03ef8a3bc3b93fc5
Dayana <dayanadiasgarcia@gmail.com>
2021-11-19 19:57:31 -0300

    Requisito 9, lint ok

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -48,25 +48,25 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     expect(getCharacter('Arya')).toEqual({
       name: 'Arya Stark',
       class: 'Rogue',
-      phrases: [ 'Not today', 'A girl has no name.' ]
+      phrases: ['Not today', 'A girl has no name.']
     });
 
     expect(getCharacter('Brienne')).toEqual({
       name: 'Brienne Tarth',
       class: 'Knight',
-      phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.' ]
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']
     });
 
     expect(getCharacter('Melissandre')).toEqual({
       name: 'Melissandre',
       class: 'Necromancer',
-      phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']
     });
 
     expect(getCharacter('arya')).toEqual({
       name: 'Arya Stark',
       class: 'Rogue',
-      phrases: [ 'Not today', 'A girl has no name.' ]
+      phrases: ['Not today', 'A girl has no name.']
     });
 
     expect(getCharacter('Dayana')).toBeUndefined();
