0546cab0bfac687c15adf854bb15e01d1b20e126
Dayana <dayanadiasgarcia@gmail.com>
2021-11-19 19:21:30 -0300

    Requisito 4 - circle - apresentando ERRO

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -21,18 +21,40 @@ const circle = require('../src/circle');
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
+// */
+
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(1)).toEqual({
+      radius: 1,
+      area: 3.14,
+      circumference: 6.28
+    });
+    expect(circle(7)).toEqual({
+      radius: 7,
+      area: 153.86,
+      circumference: 43.96
+    });
+    expect(circle(3)).toEqual({
+      radius: 3,
+      area: 28.26,
+      circumference: 18.84
+    });
+    expect(circle(typeof radius !== 'number')).toBeUndefined();
+    expect(typeof circle(2)).toBe('object');
+    expect(Object.keys(circle(2)).length).toBe(3);
+    expect(circle('')).toBeUndefined();
+    expect(circle(2)).toEqual({
+      radius: 2,
+      area: 12.56,
+      circumference: 12.56
+    });
+    expect(Object.values(circle(3))[1]).toBeCloseTo(28.26);
+    expect(circle(3)).toEqual({
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84
+    });
   });
 });
